85af63c6efa684c8c6e3e5ee6984c7685d8826f9
iesha-d <ieshad2006@gmail.com>
2022-05-10 20:19:32 +0530

    Update 2048.js

--- public/2048.js
+++ public/2048.js
@@ -7,4 +7,15 @@ class Point {
     toString() {
         return "(" + this.x + ", " + this.y + ")";
     }
+
+    translate(xShift, yShift) {
+        return new Point(this.x + xShift, this.y + yShift);
+    }
+
+    scale(factor) {
+        return new Point(this.x * factor, this.y * factor);
+    }
+
+    // In animation, we typically have a target Point that you are approaching
+    // and we want to step toward that point every frame. 
 }
