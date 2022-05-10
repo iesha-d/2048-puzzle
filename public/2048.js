fb1ab47707b983e4d71044c5789178b8e306b3f2
iesha-d <ieshad2006@gmail.com>
2022-05-10 20:05:26 +0530

    Wrote Point class

--- public/2048.js
+++ public/2048.js
@@ -1,1 +1,10 @@
-console.log("hello")
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    toString() {
+        return "(" + this.x + ", " + this.y + ")";
+    }
+}
